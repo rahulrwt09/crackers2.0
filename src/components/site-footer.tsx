830f1e366b188f059559c231208f1223ad28f21e
Rahul Rawat <Golubook09@gmail.com>
2025-07-31 17:51:41 +0530

    created new  footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -161,11 +161,17 @@ export default function SiteFooter() {
                 <span>216, Servaikaranpatty Post, Gujiliamparai, Dindigul- 624620</span>
               </li>
               <li className="flex items-center">
-                <Phone className="mr-3 h-5 w-5 shrink-0 text-red-500" />
-                <a href="tel:+919092780866" className="hover:text-white">
-                  +91 6380544276
-                </a>
-              </li>
+  <Phone className="mr-3 h-5 w-5 shrink-0 text-red-500" />
+  <a
+    href="https://wa.me/916380544276"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="hover:text-white"
+  >
+    +91 6380544276
+  </a>
+</li>
+
               <li className="flex items-center">
                 <Mail className="mr-3 h-5 w-5 shrink-0 text-red-500" />
                 <a href="mailto:nammapettikdai31@gmail.com" className="hover:text-white">
